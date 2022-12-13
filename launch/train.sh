a957fc4e1c0d776504df3e30efdfb7997e1559bd
STAR-RL <120119195+STAR-RL@users.noreply.github.com>
2022-12-13 14:16:37 +0800

    Create train.sh

---
+++ launch/train.sh
@@ -0,0 +1,4 @@
+# We first train the spatial manager (spM) and patch work (PW), run
+sh ./train_spM_PW.sh
+# Then, we train the spatial manager (spM), temporal manager (tpM) and patch work (PW) together, run
+sh ./train_all.sh
